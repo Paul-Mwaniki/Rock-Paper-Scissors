a8d1400e8832afadd3468f1a5ee31b133b215aa0
Paul Mwaniki <paulmwaniki58@gmail.com>
2023-03-28 01:35:05 +0300

    Add return to the function getComputerChoice

--- game.js
+++ game.js
@@ -2,5 +2,5 @@ function getComputerChoice(){
 
     const options = ["rock", "paper", "scissors"];
 
-    console.log(options[Math.floor(Math.random()*3)])
+    return options[Math.floor(Math.random()*3)];
 }
