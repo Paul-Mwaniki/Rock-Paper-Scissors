0064f83bdc7bae0ff2ce9a91dce92445709cab5b
Paul Mwaniki <paulmwaniki58@gmail.com>
2023-03-28 02:20:54 +0300

    *Create playRound function
    -create prompt for user to enter choice
    -store user choice
    -convert getComputerChoice function declaration to function expression
      and store in a variable so that it is accessible in the playround
      function
    -use the variable holding the getcomputerChoice function to get the
    computer random choice
    -use if else statement to give the appropiate message on the console

--- game.js
+++ game.js
@@ -1,6 +1,33 @@
-function getComputerChoice(){
+let CompChoice = function getComputerChoice(){
 
     const options = ["rock", "paper", "scissors"];
 
     return options[Math.floor(Math.random()*3)];
+};
+
+function playRound(userChoice, getComputerChoice){
+
+    let user = prompt("Enter either rock,paper or scissors");
+    console.log("User Selected: " + user);
+    let comp = CompChoice();
+    console.log("Computer Selected: " + comp);
+
+
+    if(user =="rock" && comp =="paper"){
+        console.log("Comp Wins");
+    }else if (user == "rock" && comp=="scissors"){
+        console.log("User Wins")
+    }else if (user == "paper" && comp == "scissors"){
+        console.log("Comp wins");
+    }else if (user=="paper" && comp=="rock"){
+        console.log("User wins");
+    }else if (user=="scissors" && comp=="paper"){
+        console.log("User wins");
+    }else if (user=="scissors" && comp=="rock"){
+        console.log("Comp Wins");
+    }else{
+        console.log("Draw");
+    }
+
 }
+
