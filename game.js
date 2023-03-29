4e0f187b092908916103b057adbbf3ef61412db2
Paul Mwaniki <paulmwaniki58@gmail.com>
2023-03-30 01:14:38 +0300

    * Capture score for each round

--- game.js
+++ game.js
@@ -14,20 +14,49 @@ function playRound(userChoice, getComputerChoice){
 
 
     if(user =="rock" && comp =="paper"){
-        console.log("Comp Wins");
+         console.log("Comp Wins");
+         let comp_score = 1;
+         let user_score = 0;
+         console.log("Comp score:" , comp_score);
+         console.log("User score:" , user_score);
     }else if (user == "rock" && comp=="scissors"){
         console.log("User Wins")
+        let comp_score = 0;
+        let user_score = 1;
+        console.log("Comp score:" , comp_score);
+         console.log("User score:" , user_score);
     }else if (user == "paper" && comp == "scissors"){
         console.log("Comp wins");
+        let comp_score = 1;
+        let user_score = 0;
+        console.log("Comp score:" , comp_score);
+         console.log("User score:" , user_score);
     }else if (user=="paper" && comp=="rock"){
         console.log("User wins");
+        let comp_score = 0;
+        let user_score = 1
+        console.log("Comp score:" , comp_score);
+         console.log("User score:" , user_score);
     }else if (user=="scissors" && comp=="paper"){
         console.log("User wins");
+        let comp_score = 0;
+        let user_score = 1
+        console.log("Comp score:" , comp_score);
+         console.log("User score:" , user_score);
     }else if (user=="scissors" && comp=="rock"){
         console.log("Comp Wins");
+        let comp_score = 1;
+        let user_score = 0;
+         console.log("Comp score:" , comp_score);
+         console.log("User score:" , user_score);
     }else{
         console.log("Draw");
     }
+
+}
+
+function game(){
+
 
 }
 
